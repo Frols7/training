47f02d6e7b56b84317a2688a9bbdb4ccde2ea92f
Frol Luppov <luppovfrol@gmail.com>
2021-03-01 16:53:16 +0300

    Лекция 3.4. Оптимизация переходов между страницами

--- addressbook-web-test2/addressbook-web-test2/appmanager/NavigationHelper.cs
+++ addressbook-web-test2/addressbook-web-test2/appmanager/NavigationHelper.cs
@@ -19,10 +19,18 @@ namespace WebAddressbookTest
         }
         public void OpenHomePage()
         {
+            if (driver.Url == baseURL)
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL);
         }
         public void GoToGroupPage()
         {
+            if (driver.Url == baseURL + "/group.php" && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
     }
