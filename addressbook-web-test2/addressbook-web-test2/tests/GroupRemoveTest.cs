5e8d05052766eb8844e716700400ea3bf5b5eefd
frolss <luppovfrol@live.ru>
2021-03-05 13:30:05 +0300

    Начал Лекция 4.2. Сравнение объектов и коллекций

--- addressbook-web-test2/addressbook-web-test2/tests/GroupRemoveTest.cs
+++ addressbook-web-test2/addressbook-web-test2/tests/GroupRemoveTest.cs
@@ -14,8 +14,13 @@ namespace WebAddressbookTest
         [Test]
         public void GroupRemoveTest1()
         {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
             app.Groups.CheckGroup();
             app.Groups.Remove(1);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
         }
     }
 }
